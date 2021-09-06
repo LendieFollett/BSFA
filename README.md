58e20724525e335c10623be906eaa57654605a2f
LendieFollett <lendie.follett@drake.edu>
2021-09-06 12:24:33 -0500

    update README, code

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Bayesian Stochastic Frontier Analysis (BSFA)
 
-Files needed to reproduce empirical application for "A Bayesian framework for estimating human capabilities".
+Files needed to reproduce empirical application for "A Bayesian framework for estimating human capabilities":
+
+- yop_child_data.csv: raw data describing sociodemographics of treated and untreated individuals and educational outcomes of their children.
+- bmodel_yop.stan: stan file containing model information.
+- yop_analysis.r: r file, which reads in yop_child_data.csv and compiles bmodel_yop.stan
+
+Files produced after running yop_analysis.r
+- coefficients.csv
+- functioning_counterfactual.csv
+- functioning_factual.csv
+- potentials.csv
+
 
 R packages required: rstan, ggplot2
-R packages recommended: gridExtra
 
+R packages recommended: gridExtra
--- yop_analysis.r
+++ yop_analysis.r
@@ -10,26 +10,30 @@ rstan_options(auto_write = TRUE)
 options(warn=-1)
 
 # Compile model
-compiled.model <- stan_model(file = "./bmodel_yop.stan")
+compiled.BSFA.model <- stan_model(file = "./bmodel_yop.stan")
 
 # Import data
 df <- read.csv("./yop_child_data.csv")
 
+# View data
+View(df)
 
 # Prepare data for BSFA
 N <- nrow(df)                         #N = number of rows in data
-NF <- length(unique(df$partid))       #NF = number of families / sampling units
-id <- as.numeric(as.factor(df$partid))#id= unique samping unit identifier
+NF <- length(unique(df$partid))       #NF= number of families / sampling units
+id <- as.numeric(as.factor(df$partid))#id= unique sampling unit identifier
 f <- df$child_edu_index               #f = observed functioning vector
 
+## Factual X matrix
 drop <- c("partid", "child_edu_index")
 Xf <- df[, !(names(df) %in% drop)]    #Xf= actual covariate matrix
 Xf$male_assigned <- Xf$assigned*Xf$male 
 
-Xc <- Xf                              #Xc = counterfactual covariate matrix
+## Counterfactual X matrix
+Xc <- Xf                              #Xc= counterfactual covariate matrix
 Xc$assigned <- abs(Xc$assigned - 1)   #flip treatment indicator on Xc
 Xc$male_assigned <- Xc$assigned*Xc$male
-K <- ncol(Xf)                         #number of colums in X matrix
+K <- ncol(Xf)                         #number of columns in X matrix
 
 # Compile prepared data
 data.list <- list(N = N, 
@@ -41,23 +45,30 @@ data.list <- list(N = N,
                   Xc = Xc)
 
 # Fit BSFA model
-sampled.model <- sampling(compiled.model, 
+sampled.model <- sampling(compiled.BSFA.model, 
                           data = data.list,
                           warmup = 2500,
                           iter = 5000)
 
+
+
 # Diagnostics
 traceplot(sampled.model, pars=c("mu_a", "beta", "gamma1", "gamma2", "sigma_a2", "sigma_v2", "rho"))
-summary(sampled.model, pars=c("mu_a", "beta", "gamma1", "gamma2", "sigma_a2", "sigma_v2", "rho"))$summary
-check_divergences(sampled.model)
+summary(sampled.model, pars=c("mu_a", "beta", "gamma1", "gamma2", "sigma_a2", "sigma_v2", "rho"),cache=FALSE)$summary
+
 
 # Save results
+
+# extract samples from sampled.model
 trtid <- which(colnames(Xf)%in% c("assigned", "male", "male_assigned"))
 coefficients <- data.frame(extract(sampled.model, pars = paste("beta[", trtid, "]", sep = "")))
-potentials <- data.frame(get_posterior_mean(sampled.model, pars = c("c_f"))[, 5])
 functioning_factual <- data.frame(extract(sampled.model, pars = c("f_pred_f")))
 functioning_counterfactual <- data.frame(extract(sampled.model, pars = c("f_pred_c")))
-write.csv(data, "data_sample.csv")
+
+# compute mean potentials
+potentials <- data.frame(get_posterior_mean(sampled.model, pars = c("c_f"))[, 5])
+
+# save csv files of samples
 write.csv(coefficients, "coefficients.csv")
 write.csv(potentials, "potentials.csv")
 write.csv(functioning_factual, "functioning_factual.csv")
@@ -65,66 +76,46 @@ write.csv(functioning_counterfactual, "functioning_counterfactual.csv")
 
 # Plot impact estimates
 coefficients <- read.csv(file = "coefficients.csv", header = TRUE, sep = ",")
- ggplot(data = coefficients) +
-  stat_density(aes(x = beta.1.+beta.53.), colour = "grey60", geom = "line") +
-  stat_density(aes(x = beta.1.), colour = "blue", geom = "line") +
-  theme(plot.title = element_text(hjust = 0.5)) +
+
+ggplot(data = coefficients) +
+  geom_density(aes(x = beta.1.+beta.53.,fill = "grey90"), alpha = I(.4)) +
+  geom_density(aes(x = beta.1.,fill = "grey20"),alpha = I(.4)) +
   xlab("Impact Estimate") +
-  ylab("Density") #+
-   #scale_colour_manual(name = "",
-   #                    values = c("grey60" = "grey60",
-    #                              "grey30"="grey30"),
-     #                  labels = c("females", "males"))
-
-# Plot potentials
-data <- read.csv(file = "data_sample.csv", header = TRUE, sep = ",")
-data$potentials <- read.csv(file = "potentials.csv", header = TRUE, sep = ",")[, 2]
-data.treated <- data[data$assigned == 1,]
-data.control <- data[data$assigned == 0,]
-potentials <- ggplot() + 
-  stat_ecdf(data = data.control, aes(x = potentials, colour = "c1"), geom = "line") +
-  stat_ecdf(data = data.treated, aes(x = potentials, colour = "c2"), geom = "line") +
-  ggtitle("(b)") + theme(plot.title = element_text(hjust = 0.5),  
-  legend.position = c(0.2, 0.8), legend.title = element_blank()) +
-  scale_color_manual(values = c("c1" = "grey", "c2" = "black"), 
-  labels = c("Control", "Treated")) +
-  xlab("Potential") +
-  ylab("Cumulative Probability")
-
-# Combine
-plot <- grid.arrange(coeffs, potentials, nrow = 1)
-ggsave("yop1.pdf", plot, width = 8, height = 4, units = c("in"))
-
-# Plot capability set for individual
-f_pred_f <- read.csv(file = "functioning_factual.csv", header = TRUE, sep = ",")[, -1]
-f_pred_c <- read.csv(file = "functioning_counterfactual.csv", header = TRUE, sep = ",")[, -1]
-sets1 <- ggplot() + 
-  stat_ecdf(data = f_pred_f, aes(x = f_pred_c[, 1], colour = "c1"), geom = "line") +
-  stat_ecdf(data = f_pred_f, aes(x = f_pred_f[, 1], colour = "c2"), geom = "line") +
-  ggtitle("(a)") + theme(plot.title = element_text(hjust = 0.5),  
-  legend.position = c(0.2, 0.8), legend.title = element_blank()) +
-  scale_color_manual(values = c("c1" = "grey", "c2" = "black"), 
-  labels = c("Control", "Treated")) +
-  xlab("Education Level") +
-  ylab("Cumulative Probability")
+  ylab("Density") +
+   scale_fill_manual(name = "",
+                       values = c("grey90" = "grey90",
+                                "grey20"="grey20"),
+                       labels = c("female", "male")) +
+  theme_bw()
 
 # Plot all capability sets
-data$f_pred_f_lb <- apply(f_pred_f, 2, "quantile", c(0.025))
-data$f_pred_f_ub <- apply(f_pred_f, 2, "quantile", c(0.975))
-data$f_pred_c_lb <- apply(f_pred_c, 2, "quantile", c(0.025))
-data$f_pred_c_ub <- apply(f_pred_c, 2, "quantile", c(0.975))
-data.treated <- data[data$assigned == 1,]
+
+# Capability set defined as 95% interval of sampled functionings
+# factuals
+df$f_pred_f_lb <- apply(functioning_factual, 2, "quantile", c(0.025))
+df$f_pred_f_ub <- apply(functioning_factual, 2, "quantile", c(0.975))
+# counterfactuals
+df$f_pred_c_lb <- apply(functioning_counterfactual, 2, "quantile", c(0.025))
+df$f_pred_c_ub <- apply(functioning_counterfactual, 2, "quantile", c(0.975))
+# prep data for plot
+data.treated <- df[df$assigned == 1,]
 data.treated <- data.treated[order(data.treated$f_pred_c_ub),]
 data.treated$id <- 1:nrow(data.treated)
-sets2 <- ggplot(data.treated) +
-  geom_errorbar(aes(x = id, ymin = f_pred_f_lb, ymax = f_pred_f_ub), width = 0, color = "gray") +
-  geom_line(aes(x = id, y = f_pred_c_ub)) +
-  geom_line(aes(x = id, y = f_pred_c_lb)) +
-  ggtitle("(b)") + theme(plot.title = element_text(hjust = 0.5)) +
-  xlab("Child") +
-  ylab("Education Level")
+data.treated$gender <- factor(data.treated$male, levels = c(0,1), labels = c("female", "male"))
 
-# Combine
-plot <- grid.arrange(sets1, sets2, nrow = 1)
-ggsave("yop2.pdf", plot, width = 8, height = 4, units = c("in"))
+ggplot(data.treated) +
+  geom_errorbar(aes(x = id, ymin = f_pred_f_lb, ymax = f_pred_f_ub, colour = "grey60"), width = 0) +
+  geom_errorbar(aes(x = id, ymin = f_pred_c_lb, ymax = f_pred_c_ub, colour = "grey40"), width = 0) +
+  #geom_line(aes(x = id, y = f_pred_c_ub)) +
+  #geom_line(aes(x = id, y = f_pred_c_lb)) +
+  xlab("Child") +
+  ylab("Education Level") +
+  facet_wrap(~gender) +
+  scale_y_continuous(limits = c(-5, 5))+
+  scale_colour_manual(name = "",
+                      values = c("grey60" = "grey60",
+                                 "grey40"="grey40"),
+                      labels = c("treated", "untreated")) +
+  theme_bw()
+
 
